8c05f345ef9a7ea89132754291f69f6c00c6502e
张子雍 <ziyongzhang01@gmail.com>
2024-05-12 00:21:48 +0200

    TBOX

--- scripts/graph_output.py
+++ scripts/graph_output.py
@@ -12,12 +12,12 @@ dot_stream = StringIO()
 rdf2dot(graph, dot_stream)
 
 # Save DOT representation to a file
-dot_file_path = "graph_schema.dot"
+dot_file_path = "data/graph_schema.dot"
 with open(dot_file_path, "w") as dot_file:
     dot_file.write(dot_stream.getvalue())
 
 # Generate image from DOT file
-output_image_path = "graph_schema.png"
+output_image_path = "data/graph_schema.png"
 (graph_image,) = pydot.graph_from_dot_file(dot_file_path)
 graph_image.write_png(output_image_path)
 
